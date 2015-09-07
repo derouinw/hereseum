385ecc8ca35780167539d27e0428443f67403396
Bill Derouin <derouinbill@gmail.com>
2015-09-07 15:49:51 -0700

    Move lots of code to TimelineManager

---
+++ app/src/main/java/com/grilla/hereseum/TimelineManager.java
@@ -0,0 +1,210 @@
+package com.grilla.hereseum;
+
+import android.content.Context;
+import android.location.Location;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.AbsListView;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.etiennelawlor.quickreturn.library.enums.QuickReturnViewType;
+import com.etiennelawlor.quickreturn.library.listeners.QuickReturnListViewOnScrollListener;
+import com.grilla.hereseum.adapter.PostsAdapter;
+import com.grilla.hereseum.helper.TaskCreator;
+import com.grilla.hereseum.views.TimelineView;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.List;
+
+import bolts.Continuation;
+import bolts.Task;
+
+/**
+ * Created by bill on 9/7/15.
+ */
+public class TimelineManager implements TimelineView.OnDateSelectedListener, AbsListView.OnScrollListener {
+    private static final String TAG = "TimelineManager";
+
+    private Context mContext;
+
+    private TimelineView mTimeline;
+    private RelativeLayout mEmptyArea;
+    private ListView mList;
+    private ProgressBar mLoading;
+    private TextView mFooterText;
+    private ProgressBar mFooterProgress;
+
+    private PostsAdapter mAdapter;
+
+    private Calendar mSelectedDate;
+    private boolean mLoadingMore;
+    private boolean mLocationLoaded;
+    private long mPreviousStartTime;
+
+    private String mAccessToken;
+    private Location mCurrentLocation;
+
+    public TimelineManager(View rootView, String accessToken) {
+        mContext = rootView.getContext();
+        mAccessToken = accessToken;
+        mLoadingMore = true;
+
+        setupViews(rootView);
+        setupDate();
+    }
+
+    private void setupViews(View rootView) {
+        mTimeline = (TimelineView)rootView.findViewById(R.id.timeline);
+        mTimeline.setOnDateSelectedListener(this);
+
+        mAdapter = new PostsAdapter(mContext);
+
+        mList = (ListView)rootView.findViewById(R.id.posts_list);
+        mList.setAdapter(mAdapter);
+        mList.setOnScrollListener(this);
+
+        int footerHeight = (int)(mContext.getResources().getDimension(R.dimen.months_height)
+                + mContext.getResources().getDimension(R.dimen.years_height));
+
+        QuickReturnListViewOnScrollListener scrollListener = new QuickReturnListViewOnScrollListener.Builder(QuickReturnViewType.FOOTER)
+                .footer(mTimeline)
+                .minFooterTranslation(footerHeight)
+                .isSnappable(true)
+                .build();
+        scrollListener.registerExtraOnScrollListener(this);
+        mList.setOnScrollListener(scrollListener);
+
+        View footer = LayoutInflater.from(mContext).inflate(R.layout.view_list_footer, null);
+        mFooterText = (TextView)footer.findViewById(R.id.footer_text);
+        mFooterProgress = (ProgressBar)footer.findViewById(R.id.footer_loading);
+        mList.addFooterView(footer);
+
+        mEmptyArea = (RelativeLayout)rootView.findViewById(R.id.empty_notif);
+        mLoading = (ProgressBar)rootView.findViewById(R.id.posts_loading);
+    }
+
+    // Set date to today, at beginning of day
+    private void setupDate() {
+        mSelectedDate = Calendar.getInstance();
+        mSelectedDate.set(GregorianCalendar.HOUR_OF_DAY, 0);
+        mSelectedDate.set(GregorianCalendar.MINUTE, 0);
+        mSelectedDate.set(GregorianCalendar.SECOND, 0);
+        mSelectedDate.set(GregorianCalendar.MILLISECOND,0);
+
+        mPreviousStartTime = mSelectedDate.getTimeInMillis();
+    }
+
+    public void loadPosts() {
+        mLoadingMore = true;
+
+        mList.setVisibility(View.GONE);
+        mEmptyArea.setVisibility(View.GONE);
+        mLoading.setVisibility(View.VISIBLE);
+
+        TaskCreator.getInstance(mContext).getPostsTask(mAccessToken, mCurrentLocation, mSelectedDate.getTimeInMillis())
+                .continueWith(new Continuation<List<InstaPost>, Void>() {
+                    @Override
+                    public Void then(Task<List<InstaPost>> task) throws Exception {
+                        if (task.isFaulted()) {
+                            Toast.makeText(mContext, mContext.getString(R.string.main_loading_error), Toast.LENGTH_SHORT).show();
+                            return null;
+                        }
+
+                        List<InstaPost> posts = task.getResult();
+                        mAdapter.setPosts(posts, mSelectedDate.get(Calendar.MONTH));
+
+                        mEmptyArea.setVisibility(posts.isEmpty() ? View.VISIBLE : View.GONE);
+                        mList.setVisibility(posts.isEmpty() ? View.GONE : View.VISIBLE);
+                        mList.setSelectionAfterHeaderView();
+
+                        mLoading.setVisibility(View.GONE);
+                        mLoadingMore = false;
+
+                        return null;
+                    }
+                }, Task.UI_THREAD_EXECUTOR);
+    }
+
+    public void addPosts(long startTime) {
+        mLoadingMore = true;
+
+        TaskCreator.getInstance(mContext).getPostsTask(mAccessToken, mCurrentLocation, startTime)
+                .continueWith(new Continuation<List<InstaPost>, Void>() {
+                    @Override
+                    public Void then(Task<List<InstaPost>> task) throws Exception {
+                        if (task.isFaulted()) {
+                            Toast.makeText(mContext, mContext.getString(R.string.main_loading_error), Toast.LENGTH_SHORT).show();
+                            return null;
+                        }
+
+                        List<InstaPost> posts = task.getResult();
+                        mAdapter.addPosts(posts);
+
+                        mLoadingMore = false;
+                        return null;
+                    }
+                }, Task.UI_THREAD_EXECUTOR);
+    }
+
+    public void updateLocation(Location currentLocation) {
+        mCurrentLocation = currentLocation;
+        mLocationLoaded = true;
+    }
+
+    @Override
+    public void onDateSelected(int year, int month) {
+        mSelectedDate.set(Calendar.MONTH, month);
+        mSelectedDate.set(Calendar.YEAR, year);
+        mPreviousStartTime = mSelectedDate.getTimeInMillis();
+
+        if (mLocationLoaded) {
+            loadPosts();
+        }
+    }
+
+    @Override
+    public void onScrollStateChanged(AbsListView view, int scrollState) {
+
+    }
+
+    @Override
+    public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+        int lastItem = firstVisibleItem + visibleItemCount;
+        if (lastItem == totalItemCount) {
+            // At last item, load more
+            if (!mLoadingMore) {
+                long startTime = mAdapter.getLastTime();
+                Log.d(TAG, "Loading more: " + startTime);
+
+                // Reached the end
+                if (startTime == mPreviousStartTime) {
+                    Calendar cal = Calendar.getInstance();
+                    if (mSelectedDate.get(Calendar.MONTH) == cal.get(Calendar.MONTH)
+                            && mSelectedDate.get(Calendar.YEAR) == cal.get(Calendar.YEAR)) {
+                        // current month
+                        mFooterText.setText(mContext.getString(R.string.main_footer_current));
+                        mFooterText.setVisibility(View.VISIBLE);
+                        mFooterProgress.setVisibility(View.GONE);
+                    } else {
+                        // other month
+                        mFooterText.setText(mContext.getString(R.string.main_footer_old));
+                        mFooterText.setVisibility(View.VISIBLE);
+                        mFooterProgress.setVisibility(View.GONE);
+                    }
+                } else {
+                    // loading
+                    mFooterText.setVisibility(View.GONE);
+                    mFooterProgress.setVisibility(View.VISIBLE);
+                    mPreviousStartTime = startTime;
+                    addPosts(startTime);
+                }
+            }
+        }
+    }
+}
--- app/src/main/java/com/grilla/hereseum/activities/MainActivity.java
+++ app/src/main/java/com/grilla/hereseum/activities/MainActivity.java
@@ -20,6 +20,7 @@ import com.etiennelawlor.quickreturn.library.enums.QuickReturnViewType;
 import com.etiennelawlor.quickreturn.library.listeners.QuickReturnListViewOnScrollListener;
 import com.grilla.hereseum.InstaPost;
 import com.grilla.hereseum.R;
+import com.grilla.hereseum.TimelineManager;
 import com.grilla.hereseum.adapter.PostsAdapter;
 import com.grilla.hereseum.helper.TaskCreator;
 import com.grilla.hereseum.views.TimelineView;
@@ -30,30 +31,20 @@ import java.util.List;
 import bolts.Continuation;
 import bolts.Task;
 
-public class MainActivity extends AppCompatActivity implements
-        AbsListView.OnScrollListener, TimelineView.OnDateSelectedListener {
+public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
 
     public static final String EXTRA_ACCESS_TOKEN = "access_token";
 
     private String mAccessToken;
-    private Location mCurrentLocation;
+
     private boolean mLoadedFirst;
-    private boolean mLoadingMore;
-    private long mPreviousStartTime;
-    private int mYear, mMonth;
 
-    private TimelineView mTimeline;
-    private RelativeLayout mEmptyArea;
-    private ListView mList;
-    private ProgressBar mLoading;
-    private TextView mFooterText;
-    private ProgressBar mFooterProgress;
-
-    private PostsAdapter mAdapter;
+    private TimelineManager mTimelineManager;
 
     private LocationManager mLocationManager;
     private LocationListener mLocationListener;
+    private Location mCurrentLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,38 +52,7 @@ public class MainActivity extends AppCompatActivity implements
         setContentView(R.layout.activity_main);
 
         mAccessToken = getIntent().getStringExtra(EXTRA_ACCESS_TOKEN);
-
-        mTimeline = (TimelineView)findViewById(R.id.timeline);
-        mTimeline.setOnDateSelectedListener(this);
-
-        mLoadingMore = true;
-        mAdapter = new PostsAdapter(this);
-        mList = (ListView)findViewById(R.id.posts_list);
-        mList.setAdapter(mAdapter);
-        mList.setOnScrollListener(this);
-
-        int footerHeight = (int)(getResources().getDimension(R.dimen.months_height) + getResources().getDimension(R.dimen.years_height));
-        QuickReturnListViewOnScrollListener scrollListener = new QuickReturnListViewOnScrollListener.Builder(QuickReturnViewType.FOOTER)
-                .footer(mTimeline)
-                .minFooterTranslation(footerHeight)
-                .isSnappable(true)
-                .build();
-        scrollListener.registerExtraOnScrollListener(this);
-        mList.setOnScrollListener(scrollListener);
-
-        View footer = LayoutInflater.from(this).inflate(R.layout.view_list_footer, null);
-        mFooterText = (TextView)footer.findViewById(R.id.footer_text);
-        mFooterProgress = (ProgressBar)footer.findViewById(R.id.footer_loading);
-        mList.addFooterView(footer);
-
-        mEmptyArea = (RelativeLayout)findViewById(R.id.empty_notif);
-        mLoading = (ProgressBar)findViewById(R.id.posts_loading);
-
-        mPreviousStartTime = 0;
-
-        Calendar cal = Calendar.getInstance();
-        mMonth = cal.get(Calendar.MONTH);
-        mYear = cal.get(Calendar.YEAR);
+        mTimelineManager = new TimelineManager(findViewById(R.id.root_view), mAccessToken);
 
         setupLocation();
     }
@@ -100,7 +60,6 @@ public class MainActivity extends AppCompatActivity implements
     @Override
     public void onResume() {
         super.onResume();
-
         mLocationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, mLocationListener);
     }
 
@@ -108,18 +67,6 @@ public class MainActivity extends AppCompatActivity implements
     public void onPause() {
         mLocationManager.removeUpdates(mLocationListener);
         super.onPause();
-    }
-
-    private long getTime(int year, int month) {
-        Calendar startDate = Calendar.getInstance();
-        startDate.set(Calendar.YEAR, year);
-        startDate.set(Calendar.MONTH, month);
-        startDate.set(Calendar.DAY_OF_MONTH, 0); // ¯\_(ツ)_/¯
-        return startDate.getTimeInMillis()/1000;
-    }
-
-    private void showPosts(int year, int month) {
-        loadPosts(getTime(year, month));
     }
 
     private void setupLocation() {
@@ -144,106 +91,5 @@ public class MainActivity extends AppCompatActivity implements
 
             public void onProviderDisabled(String provider) {}
         };
-    }
-
-    private void loadPosts(long startTime) {
-        mLoadingMore = true;
-        mPreviousStartTime = startTime;
-
-        mList.setVisibility(View.GONE);
-        mEmptyArea.setVisibility(View.GONE);
-        mLoading.setVisibility(View.VISIBLE);
-
-        TaskCreator.getInstance(this).getPostsTask(mAccessToken, mCurrentLocation, startTime).continueWith(new Continuation<List<InstaPost>, Void>() {
-            @Override
-            public Void then(Task<List<InstaPost>> task) throws Exception {
-                if (task.isFaulted()) {
-                    Toast.makeText(MainActivity.this, getString(R.string.main_loading_error), Toast.LENGTH_SHORT).show();
-                    return null;
-                }
-
-                List<InstaPost> posts = task.getResult();
-                mAdapter.setPosts(posts, mMonth);
-
-                mEmptyArea.setVisibility(posts.isEmpty() ? View.VISIBLE : View.GONE);
-                mList.setVisibility(posts.isEmpty() ? View.GONE : View.VISIBLE);
-                mList.setSelectionAfterHeaderView();
-
-                mLoading.setVisibility(View.GONE);
-                mLoadingMore = false;
-
-                return null;
-            }
-        }, Task.UI_THREAD_EXECUTOR);
-    }
-
-    private void addPosts(long startTime) {
-        mLoadingMore = true;
-        mPreviousStartTime = startTime;
-
-        TaskCreator.getInstance(this).getPostsTask(mAccessToken, mCurrentLocation, startTime).continueWith(new Continuation<List<InstaPost>, Void>() {
-            @Override
-            public Void then(Task<List<InstaPost>> task) throws Exception {
-                if (task.isFaulted()) {
-                    Toast.makeText(MainActivity.this, getString(R.string.main_loading_error), Toast.LENGTH_SHORT).show();
-                    return null;
-                }
-
-                List<InstaPost> posts = task.getResult();
-                mAdapter.addPosts(posts);
-
-                mLoadingMore = false;
-                return null;
-            }
-        }, Task.UI_THREAD_EXECUTOR);
-    }
-
-    @Override
-    public void onScrollStateChanged(AbsListView view, int scrollState) {
-
-    }
-
-    @Override
-    public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
-        int lastItem = firstVisibleItem + visibleItemCount;
-        if (lastItem == totalItemCount) {
-            // At last item, load more
-            if (!mLoadingMore) {
-                long startTime = mAdapter.getLastTime();
-                Log.d(TAG, "Loading more: " + startTime);
-
-                if (startTime == mPreviousStartTime) {
-                    Calendar cal = Calendar.getInstance();
-                    if (mMonth == cal.get(Calendar.MONTH) && mYear == cal.get(Calendar.YEAR)) {
-                        // current month
-                        mFooterText.setText(getString(R.string.main_footer_current));
-                        mFooterText.setVisibility(View.VISIBLE);
-                        mFooterProgress.setVisibility(View.GONE);
-                    } else {
-                        // other month
-                        mFooterText.setText(getString(R.string.main_footer_old));
-                        mFooterText.setVisibility(View.VISIBLE);
-                        mFooterProgress.setVisibility(View.GONE);
-                    }
-                } else {
-                    // loading
-                    mFooterText.setVisibility(View.GONE);
-                    mFooterProgress.setVisibility(View.VISIBLE);
-                    addPosts(startTime);
-                }
-            }
-        }
-    }
-
-    @Override
-    public void onDateSelected(int year, int month) {
-        mYear = year;
-        mMonth = month;
-
-        if (mLoadedFirst) {
-            showPosts(year, month);
-        } else {
-            mPreviousStartTime = getTime(year, month);
-        }
     }
 }
